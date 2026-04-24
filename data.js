6431ba2bc315abebfdc04b51dc89f7853265db1b
PRAJA ADITYA <adityaputraa092@gmail.com>
2026-04-24 09:22:39 +0700

    upload github pertamaku

---
+++ data.js
@@ -0,0 +1,20 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "sekali slank tetap slank",
+        keahlian: ["javascript", "php"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNcN0FELjRVij4hIjJhsc49jBo2RQsILNgbA&s",
+        latar: "ini adalah proyek magang yamg saya kerjakan di pt slank dunia",
+        tantangan: "kebutuhan dari pt-nya cukup enak",
+        pelajaran: "saya mendapatkan pelajaran bahwa saya harus bisa bekerja sama dengan tim",
+        link: ""
+    },
+ "sistem_pos_xyz":{
+        judul: "sekali slank tetap slank",
+        keahlian: ["javascript", "php"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNcN0FELjRVij4hIjJhsc49jBo2RQsILNgbA&s",
+        latar: "ini adalah proyek magang yamg saya kerjakan di pt slank dunia",
+        tantangan: "kebutuhan dari pt-nya cukup enak",
+        pelajaran: "saya mendapatkan pelajaran bahwa saya harus bisa bekerja sama dengan tim",
+        link: ""
+    },
+};
